7292f53c8c768b650793d5cc97ac1390dd446b40
Artem <DeveloperJefremov@gmail.com>
2024-04-06 22:15:34 +0300

    publishing

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,127 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="app">
+        <h1>Страница с постами</h1>
+        <my-input
+        v-model="searchQuery"
+        placeholder="Поиск..."
+        />
+        <div class="app__btns">
+            <my-button @click="showDialog">Создать пользователя</my-button>
+            <my-select v-model="selectedSort" :options="sortOptions"/>
+        </div>
+        <my-dialog v-model:show="dialogVisible">
+            <post-form @create="createPost"/>
+        </my-dialog>
+        <post-list :posts="sortedAndSearchedPosts" @remove="removePost" v-if="!isPostsLoading"/>
+        <div v-else>Идет загрузка...</div>
+        <div class="page__wrapper">
+            <div v-for="pageNumber in totalPages" 
+            :key="pageNumber" 
+            class="page" 
+            :class="{'currentPage': page === pageNumber
+        }"
+            >
+            {{ pageNumber }}
+        </div>
+        </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostForm from "./components/PostForm";
+import PostList from "@/components/PostList";
+import MyButton from "@/components/UI/MyButton";
+import axios from 'axios';
+import MySelect from "@/components/UI/MySelect";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+export default { 
+    components:{
+        MySelect,
+        MyButton,
+        PostList, PostForm
+    },
+    data() {
+        return {
+            posts: [],
+            dialogVisible: false,
+            isPostsLoading: false,
+            selectedSort: '',
+            searchQuery:'',
+            page: 1,
+            limit: 10,
+            totalPages: 0,
+            sortOptions: [
+                {value: 'title', name: 'По названию'},
+                {value: 'body', name: 'По содержанию'},
+            ]
+        }
+    },
+methods: {
+    createPost(post){
+        this.posts.push(post);
+        this.dialogVisible = false;
+        },
+        removePost(post) {
+            this.posts = this.posts.filter(p => p.id !==post.id)
+        },
+        showDialog() {
+            this.dialogVisible = true;
+        },
+        async fetchPosts() {
+            try {
+                this.isPostsLoading = true;
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts',{
+                    params:{_page: this.page,
+                    _limit: this.limit}
+                });
+                this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
+                this.posts = response.data;
+                this.isPostsLoading = false;
+            } catch (e) {
+                alert('Ошибка')
+            } 
+        }
+    },
+        mounted() {
+            this.fetchPosts();
+    },
+    computed: {
+        sortedPosts() {
+            return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+        },
+        sortedAndSearchedPosts(){
+            return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
+        }
+    },
+    watch: {
+        
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.app {
+    padding: 20px;
+}
+.app__btns {
+    display: flex;
+    justify-content: space-between;
+    margin: 15px 0;
+}
+.page__wrapper {
+    display: flex;
+    margin-top: 15px;
+}
+.page {
+    border: 1px solid black;
+    padding: 10px;
+}
+.current-page{
+    border: 2px solid teal;
 }
 </style>
